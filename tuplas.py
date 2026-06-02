4372627f44c48ad8d51e2cb4109c8a98486f51aa
Santiago Poustis <Santiagopoustis21@gmail.com>
2026-06-02 20:31:46 -0300

    Agrego modulo de tuplas

---
+++ tuplas.py
@@ -0,0 +1,25 @@
+#variable = (23, 43, 12, 54, 34)
+#
+#for i in range(len(variable)):
+#    print(variable[i])
+#
+#tupla_un_valor = (23,)
+#
+#print(type(tupla_un_valor))
+
+
+#paises = ("Argentina", "Bolivia", ("Hola", 67, 87), "Chile", "Paraguay", "Peru", "Uruguay")
+#
+#print(paises[-1])
+
+#tupla_de_dias = ("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo")
+#print(tupla_de_dias)
+#print(id(tupla_de_dias))
+#
+#tupla_de_dias = ("Lunes", "Martes", "Miercoles", "Jueves", "Viernes")
+#print(tupla_de_dias)
+#print(id(tupla_de_dias))
+
+tupla1 = (2, 3, 4, 5) * 4
+
+print(tupla1)
